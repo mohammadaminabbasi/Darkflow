a38e855107e748325d38144a7998f73bd4791b4d
mohammadaminabbasi <ma.abbasi1378@yahoo.com>
2022-06-11 23:51:32 +0430

    likes

--- user_activity/urls.py
+++ user_activity/urls.py
@@ -8,6 +8,7 @@ router = routers.DefaultRouter()
 urlpatterns = [
     path('like_song/', like_song, name="like_song"),
     path('unlike_song/', unlike_song, name="unlike_song"),
+    path('is_song_liked_by_user/', is_song_liked_by_user, name="is_song_liked_by_user"),
     path('comment_song/', comment_song, name="comment_song"),
     path('get_all_song_comments/', get_all_song_comments, name="comment_song"),
 ]
